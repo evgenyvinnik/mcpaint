f49219ba6a63984af2d1e89aaf185d478dbd4da5
Evgeny Vinnik <evgenyvinnik@gmail.com>
2026-07-22 20:11:09 -0700

    Revert "Merge pull request #15 from evgenyvinnik/chore/major-dependency-upgrades"
    This reverts commit beb04a832bf371f744ac2456331800005ed097df, reversing
    changes made to a3ea932b5400c8342fc3f855e3402149deaa6b7c.

--- src/react/components/SelectionHandles.tsx
+++ src/react/components/SelectionHandles.tsx
@@ -283,8 +283,8 @@ export function SelectionHandles({ selection, onResize, containerRef, handleSize
 
   // Calculate handle positions
   const getHandleStyle = (xAxis: HandleAxis, yAxis: HandleAxis): CSSProperties => {
-    let left: number;
-    let top: number;
+    let left = scaledX;
+    let top = scaledY;
 
     // X position
     if (xAxis === HANDLE_START) {
